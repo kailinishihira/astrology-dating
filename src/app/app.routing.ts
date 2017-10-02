29b2702d57f8b8da5cb3b74839d958ad0529cd7f
Lois Choi <loisch22@gmail.com>
2017-10-02 12:29:22 -0700

    look for error in home view

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { HomeComponent} from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
 
+
 const appRoutes: Routes = [
   {
     path: '',
